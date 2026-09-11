80a5654b9042822a3cec7f3c134237d580520733
gaaaaaaabriel <gg405246@gmail.com>
2026-09-11 00:02:53 -0300

    subindo com arquivos para poder subir a arquitetura de pastas

---
+++ frontEnd/leva_ai/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'core/di/injecao_dependencias.dart';
+import 'core/routes/rotas_app.dart';
+import 'core/theme/tema_app.dart';
+
+void main() => runApp(const AplicacaoLevaAi());
+
+class AplicacaoLevaAi extends StatelessWidget {
+  const AplicacaoLevaAi({super.key});
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'LevaAí',
+    debugShowCheckedModeBanner: false,
+    theme: TemaApp.claro,
+    initialRoute: RotasApp.inicio,
+    onGenerateRoute: InjecaoDependencias.gerarRota,
+  );
+}
